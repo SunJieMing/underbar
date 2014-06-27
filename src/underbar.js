5a6633d4263b260a9829236926d3e79930d6db10
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-06-27 16:58:07 -0600

    Completed _.contains

--- src/underbar.js
+++ src/underbar.js
@@ -189,12 +189,29 @@ var _ = {};
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
-    return _.reduce(collection, function(wasFound, item) {
-      if (wasFound) {
-        return true;
-      }
-      return item === target;
-    }, false);
+    if(collection.length === undefined){
+        for (var key in collection) {
+          if (collection.hasOwnProperty(key)) {
+              if(collection[key] === target){
+                   return true;   
+              }
+          }
+        }
+        return false;
+    }
+    for(var i = 0; i < collection.length; i++){
+        if(collection[i] === target){
+         return true;   
+        }
+    }
+    return false;
+
+    // return _.reduce(collection, function(wasFound, item) {
+    //   if (wasFound) {
+    //     return true;
+    //   } else if (!wasFound){return false;}
+    //   return item === target;
+    // }, false);
   };
 
 
