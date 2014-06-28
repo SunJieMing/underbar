33b3d215782ad92ed246b96fb6acd888f678cbb3
Ben Nelson <s.benjamin.nelson@gmail.com>
2014-06-28 15:57:20 -0600

    I finished the _.memoize function.

--- src/underbar.js
+++ src/underbar.js
@@ -330,6 +330,14 @@ var _ = {};
   // already computed the result for the given argument and return that value
   // instead if possible.
   _.memoize = function(func) {
+    var resultCache = {};        
+    return function(arg){
+        if(arg in resultCache){
+            return resultCache[arg];
+        }
+        resultCache[arg] = func(arg);
+        return resultCache[arg];
+    };
   };
 
   // Delays a function for the given number of milliseconds, and then calls
